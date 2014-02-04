de57f1d80a86a17208e3f34c675f0e50cd73c9e2
Charles Lehner <celehner1@gmail.com>
2014-02-04 12:28:06 -0500

    Fix header file formatting and missing line

--- chains.h
+++ chains.h
@@ -8,8 +8,12 @@
 
 struct markov_model;
 
+// creation
+
 struct markov_model*
 mm_new();
+
+// inspection
 
 void
 mm_print(struct markov_model *model);
@@ -31,15 +35,21 @@ mm_learn_file(struct markov_model *model, char *corpus_path);
 // generating
 
 int
-mm_pick_ngram(struct markov_model *model, char **ngram, unsigned int n, int direction);
+mm_pick_ngram(struct markov_model *model, char **ngram,
+	unsigned int n, int direction);
 
 void
 mm_generate_sequence(struct markov_model *model, int n,
+	char *sequence[], int *seq_len, int direction);
 
 int
-mm_generate_sentence(struct markov_model *model, const char **initial_ngram, char *sentence);
+mm_generate_sentence(struct markov_model *model,
+	const char **initial_ngram, char *sentence);
+
+// both
 
 int
-mm_respond_and_learn(struct markov_model *model, const char line[], char *best_response, char learn);
+mm_respond_and_learn(struct markov_model *model, const char line[],
+	char *best_response, char learn);
 
 #endif /* CHAINS_H */
